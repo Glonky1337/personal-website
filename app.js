48be439d32fd25e54c371da58338da47ef77adfc
Glonky1337 <fergusontbs@gmail.com>
2023-05-14 19:53:16 -0400

    Add light and dark mode option functionality

--- app.js
+++ app.js
@@ -16,3 +16,32 @@ function toggleHamburger() {
     x.checked = false;
   }
 }
+
+function lightDarkMode() {
+    let x = document.getElementById("light-dark-mode");
+    let body = document.getElementById("body");
+    let linkedinContactLogo = document.querySelector(".contact.linkedin");
+    let githubContactLogo = document.querySelector(".contact.github");
+
+    if (x.checked) {
+        console.log('light-mode');
+        body.style.color = "rgb(17, 24, 39)";
+        body.style.backgroundColor = "rgb(255, 255, 255)";
+        linkedinContactLogo.style.backgroundImage = "url(./images/section-icons/linkedin-contact-dark.png)";
+        githubContactLogo.style.backgroundImage = "url(./images/section-icons/github-contact-dark.png)";
+        // x.checked = false;
+    } else {
+        console.log('dark-mode');
+        body.style.color = "";
+        body.style.backgroundColor = "";
+        linkedinContactLogo.style.backgroundImage = "";
+        githubContactLogo.style.backgroundImage = "";
+    }
+}
+
+function changeBackgroundColorTextColor(variable, backgroundColor, color) {
+    for (let i = 0; i < variable.length; i++) {
+        variable[i].style.backgroundColor = backgroundColor;
+        variable[i].style.color = color;
+    }
+}
