e0adda3383c708f327d706cc55ac356d6c19907d
Glonky1337 <fergusontbs@gmail.com>
2023-05-21 16:20:43 -0400

    Add character limit validation to contact form fields
    - Limit first name and last name fields to 50 characters
    - Restrict email field to a maximum of 100 characters
    - Enforce a maximum of 15 characters for phone number field
    - Set message field to allow a maximum of 200 characters
    
    Closes #1

--- php/contact-form.php
+++ php/contact-form.php
@@ -6,15 +6,44 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $phoneNumber = sanitizeInput($_POST["phoneNumber"]);
     $message = sanitizeInput($_POST["message"]);
 
+    // Validate character limits
+    $maxFirstNameLength = 50;
+    $maxLastNameLength = 50;
+    $maxEmailLength = 100;
+    $maxPhoneNumberLength = 15;
+    $maxMessageLength = 200;
+
+    $errors = array();
+
     if (empty($firstName) || empty($lastName) || empty($email) || empty($phoneNumber) || empty($message)) {
-        http_response_code(400); // "Bad Request"
-        echo "All fields are required.";
-        exit;
+        $errors[] = "All fields are required.";
     }
 
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        http_response_code(400);
-        echo "Invalid email address.";
+    if (strlen($firstName) > $maxFirstNameLength) {
+        $errors[] = "First name should not exceed {$maxFirstNameLength} characters.";
+    }
+
+    if (strlen($lastName) > $maxLastNameLength) {
+        $errors[] = "Last name should not exceed {$maxLastNameLength} characters.";
+    }
+
+    if (strlen($email) > $maxEmailLength) {
+        $errors[] = "Email should not exceed {$maxEmailLength} characters.";
+    } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $errors[] = "Invalid email address.";
+    }
+
+    if (strlen($phoneNumber) > $maxPhoneNumberLength) {
+        $errors[] = "Phone number should not exceed {$maxPhoneNumberLength} characters.";
+    }
+
+    if (strlen($message) > $maxMessageLength) {
+        $errors[] = "Message should not exceed {$maxMessageLength} characters.";
+    }
+
+    if (!empty($errors)) {
+        http_response_code(400); // Bad Request
+        echo implode("<br>", $errors);
         exit;
     }
 
@@ -24,11 +53,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             . "Last Name: " . $lastName . "\n"
             . "Email: " . $email . "\n"
             . "Message: " . $message;
-    
+
     $headers = "Form: " . $email . "\r\n"
             . "Reply-To: " . $email . "\r\n"
             . "X-Mailer: PHP/" . phpversion();
-    
+
     if (mail($to, $subject, $message, $headers)) {
         // Email sent successfully
         http_response_code(200); // OK
