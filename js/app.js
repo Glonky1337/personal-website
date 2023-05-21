50c1ef5158b3db5d4af792362085819fbeaab9f6
Glonky1337 <fergusontbs@gmail.com>
2023-05-21 15:41:27 -0400

    Refactor function to toggle website theme color

--- js/app.js
+++ js/app.js
@@ -17,29 +17,56 @@ function toggleHamburger() {
   }
 }
 
-function lightDarkMode() {
-    let x = document.getElementById("light-dark-mode");
-    let body = document.getElementById("body");
-    let linkedinContactLogo = document.querySelector(".contact.linkedin");
-    let githubContactLogo = document.querySelector(".contact.github");
+// WEBSITE COLOR THEME
+// Toggle between light and dark modes
+function toggleColorMode() {
+  const currentColorMode = getCurrentColorMode();
 
-    if (x.checked) {
-        body.style.color = "rgb(17, 24, 39)";
-        body.style.backgroundColor = "rgb(255, 255, 255)";
-        linkedinContactLogo.style.backgroundImage = "url(/images/section-icons/linkedin-contact-dark.png)";
-        githubContactLogo.style.backgroundImage = "url(/images/section-icons/github-contact-dark.png)";
-        // x.checked = false;
-    } else {
-        body.style.color = "";
-        body.style.backgroundColor = "";
-        linkedinContactLogo.style.backgroundImage = "";
-        githubContactLogo.style.backgroundImage = "";
-    }
+  if (currentColorMode === 'light') {
+    setColorMode('dark');
+  } else {
+    setColorMode('light');
+  }
 }
 
-function changeBackgroundColorTextColor(variable, backgroundColor, color) {
-    for (let i = 0; i < variable.length; i++) {
-        variable[i].style.backgroundColor = backgroundColor;
-        variable[i].style.color = color;
-    }
+// Get the current color mode from local storage
+function getCurrentColorMode() {
+  const preferredColorMode = localStorage.getItem('colorMode');
+
+  if (preferredColorMode === 'light') {
+    return 'light';
+  } else {
+    return 'dark';
+  }
 }
+
+// Set the preferred color mode in local storage
+function setColorMode(colorMode) {
+  localStorage.setItem('colorMode', colorMode);
+  applyColorMode(colorMode);
+}  // const bodyElement = document.body;
+
+// Apply color mode to the page
+function applyColorMode(colorMode) {
+  let body = document.getElementById("body");
+  let linkedinContactLogo = document.querySelector(".contact.linkedin");
+  let githubContactLogo = document.querySelector(".contact.github");
+
+  if (colorMode === 'light') {
+    body.style.color = "rgb(17, 24, 39)";
+    body.style.backgroundColor = "rgb(255, 255, 255)";
+    linkedinContactLogo.style.backgroundImage = "url(/images/section-icons/linkedin-contact-dark.png)";
+    githubContactLogo.style.backgroundImage = "url(/images/section-icons/github-contact-dark.png)";
+  } else {
+    body.style.color = "";
+    body.style.backgroundColor = "";
+    linkedinContactLogo.style.backgroundImage = "";
+    githubContactLogo.style.backgroundImage = "";
+  }
+}
+
+// On page load, apply the stored color mode if available
+window.addEventListener('load', function () {
+  const preferredColorMode = getCurrentColorMode();
+  applyColorMode(preferredColorMode);
+});
